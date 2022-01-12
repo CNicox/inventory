9ca75b2ade270400d195732e11814c7855a2aef4
“nicolai.comar” <nicolai.comar@unbelievable-machine.com>
2022-01-12 17:17:11 +0100

    added initial models, created db, added moels to amin

--- inventory/admin.py
+++ inventory/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import *
+
+"""
+how to create admin console
+python manage.py createsuperuser admin admin500!
+"""
+
+admin.site.register(Item)
+admin.site.register(Comment)
+admin.site.register(Person)
+admin.site.register(Equipment_category)
+admin.site.register(Department)
+
+
+
 
 # Register your models here.
--- inventory/models.py
+++ inventory/models.py
@@ -1,3 +1,66 @@
 from django.db import models
-
+from datetime import datetime
 # Create your models here.
+#how to apply changes to the database (to models):
+#python manage.py makemigrations (commit changes)
+#python manage.py migrate (applies)
+
+
+class Item(models.Model):
+    name = models.CharField(max_length=128)
+    item_model = models.CharField(max_length=128, blank=True)
+    order_number = models.CharField(max_length=64, blank=True)
+    model_number = models.CharField(max_length=64, blank=True)
+    serial_number = models.CharField(max_length=128, blank=True)
+    properties =  models.CharField(max_length=500, blank=True)
+    assignment_date = models.DateTimeField(blank=True) # need to configure so that this field refreshes each time a person field is changed
+    notes = models.CharField(max_length=1000, blank=True)
+    #here are those additional fields
+    pin = models.CharField(max_length=10, blank=True)
+    puk = models.CharField(max_length=10, blank=True)
+    mobile_number = models.CharField(max_length=24, blank=True)
+
+    owner = models.ForeignKey("Person", on_delete=models.SET_NULL, blank=True, null=True, related_name="items")
+    equipment_category = models.ForeignKey('Equipment_category', on_delete=models.SET_NULL, null=True,
+                                           related_name="items")  # if Equipment_category is deleted the value is set to 0 and items is how the field is referenced from Equipment_category
+    department = models.ForeignKey('Department', on_delete=models.SET_NULL, null=True, related_name="items")
+
+
+class Comment(models.Model):
+    text = models.CharField(max_length=512)
+    author = models.CharField(max_length=128)
+    published = models.DateTimeField(default=datetime.now)
+    item = models.ForeignKey(Item, on_delete=models.CASCADE, related_name='comments')
+
+
+class Person(models.Model):
+    first_name = models.CharField(max_length=128)
+    last_name = models.CharField(max_length=128)
+    mobile_number = models.CharField(max_length=24, blank=True)
+    email = models.EmailField(max_length=254, blank=True)
+
+
+
+class Equipment_category(models.Model):
+    name = models.CharField(max_length=128)
+
+class Department(models.Model):
+    name = models.CharField(max_length=128)
+
+
+
+
+
+
+
+
+'''
+as info for on_delete
+CASCADE: When the referenced object is deleted, also delete the objects that have references to it (when you remove a blog post for instance, you might want to delete comments as well). SQL equivalent: CASCADE.
+PROTECT: Forbid the deletion of the referenced object. To delete it you will have to delete all objects that reference it manually. SQL equivalent: RESTRICT.
+RESTRICT: (introduced in Django 3.1) Similar behavior as PROTECT that matches SQL's RESTRICT more accurately. (See django documentation example)
+SET_NULL: Set the reference to NULL (requires the field to be nullable). For instance, when you delete a User, you might want to keep the comments he posted on blog posts, but say it was posted by an anonymous (or deleted) user. SQL equivalent: SET NULL.
+SET_DEFAULT: Set the default value. SQL equivalent: SET DEFAULT.
+SET(...): Set a given value. This one is not part of the SQL standard and is entirely handled by Django.
+DO_NOTHING: Probably a very bad idea since this would create integrity issues in your database (referencing an object that actually doesn't exist). SQL equivalent: NO ACTION.
+'''
